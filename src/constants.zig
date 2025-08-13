3b3d120e89c15f781b93883d86a412ab1691ac0a
Sam Mercer <130914459+smercer10@users.noreply.github.com>
2025-08-13 19:13:44 +0100

    Create bitboard helpers

---
+++ src/constants.zig
@@ -0,0 +1,3 @@
+pub const num_squares = 64;
+pub const num_ranks = 8;
+pub const num_files = 8;
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,21 @@
 const std = @import("std");
+const t = @import("types.zig");
 
 pub fn main() void {
-    std.debug.print("Hello, World!\n", .{});
+    var bb = t.Bitboard.fromU64(0);
+    const sq = t.Square.a1;
+    std.debug.print("Is {} set? {}\n", .{ sq, bb.isSet(sq) });
+    std.debug.print("Least significant bit set: {?}\n", .{bb.getLsb()});
+    bb.set(sq);
+    std.debug.print("Is {} set? {}\n", .{ sq, bb.isSet(sq) });
+    std.debug.print("Least significant bit set: {?}\n", .{bb.getLsb()});
+    bb.clear(sq);
+    std.debug.print("Is {} set? {}\n", .{ sq, bb.isSet(sq) });
+    bb.set(sq);
+    std.debug.print("Is {} set? {}\n", .{ sq, bb.isSet(sq) });
+    std.debug.print("Least significant bit set: {?}\n", .{bb.popLsb()});
+    std.debug.print("Is {} set? {}\n", .{ sq, bb.isSet(sq) });
+    bb.set(sq);
+    bb.set(.h8);
+    bb.print();
 }
